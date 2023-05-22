f3ddb2df0d374981ca7a10e98b320706a4c3cf5f
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-05-22 23:34:16 +0530

    Added:Delete Existing Contact Person

---
+++ src/com/bridgelabz/Uc4/AddressBookMain.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.Uc4;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Welcome to AddressBook program");
+            System.out.println("1) Press 1 to add new contact");
+            System.out.println("2) press 2 to display contact");
+            System.out.println("3) press 3 to Edit contact");
+            System.out.println("4) press 4 to delete contact");
+            System.out.println("5) press 5 to Exit");
+            int input = sc.nextInt();
+            switch (input){
+                case 1 -> {
+                    addressBook.AddContact();
+                }
+                case 2 -> {
+                    addressBook.display();
+                }
+                case 3 -> {
+                    addressBook.Edit();
+                }
+                case 4 -> {
+                    addressBook.delete();
+                }
+                case 5 ->{
+                    System.out.println("Thank you so much !!!");
+                    System.exit(0);
+                }
+
+
+            }
+        }
+    }
+}
