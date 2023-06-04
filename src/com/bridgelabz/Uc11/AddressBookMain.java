4a0ab93a4716ea36a98a5f878ff9e180716b9c02
pallavikabadi <58931162+pallavikabadi@users.noreply.github.com>
2023-06-04 13:03:45 +0530

    Update AddressBookMain.java
    Added:Sort Alphabetically By Person Name

--- src/com/bridgelabz/Uc11/AddressBookMain.java
+++ src/com/bridgelabz/Uc11/AddressBookMain.java
@@ -161,7 +161,7 @@ public class AddressBookMain extends Collection {
         Scanner scanner = new Scanner(System.in);
         while (true){
             System.out.println("---Address Book---");
-            System.out.println("1. Add AddressBook \n2. Add Contact \n3. Edit Contact \n4. Delete Contact \n5. Display Contact \n6. Search State \n7. Search City\n");
+            System.out.println("1. Add AddressBook \n2. Add Contact \n3. Edit Contact \n4. Delete Contact \n5. Display Contact \n6. Search State \n7. Search City\n8. Sort By Person's Name");
             System.out.println("Choose any Number : ");
 
             int choice = scanner.nextInt();
