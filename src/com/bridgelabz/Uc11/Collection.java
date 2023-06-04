57a7b190937bf30fd00d30954edd26573b2baf6b
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-06-04 12:37:34 +0530

    Added:Sort Alphabetically By Person Name

---
+++ src/com/bridgelabz/Uc11/Collection.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.Uc11;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Collection {
+    /*
+      Create ArrayList of ContactPerson object called contact
+      for store the contact of each person.
+   */
+    ArrayList<Contact> contacts = new ArrayList<>();
+    public static Map<String, AddressBook> addressBookMap= new HashMap<>();
+
+}
---
+++ src/com/bridgelabz/Uc8/AddressBook.java
@@ -0,0 +1,190 @@
+package com.bridgelabz.Uc8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+public class AddressBook extends Collection {
+    /*
+    Create ArrayList of Contact object called contact
+    for store the contact of each person.
+ */
+    public ArrayList<Contact> getContact() {
+        return contacts;
+    }
+    public void setContact(ArrayList<Contact> contact){
+        this.contacts = contact;
+    }
+
+    private String addressBookName;
+
+    public AddressBook(String addressBookName) {
+
+        this.addressBookName = addressBookName;
+    }
+    public AddressBook() {
+
+    }
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "contacts=" + contacts +
+                ", addressBookName='" + addressBookName + '\'' +
+                "}\n";
+    }
+    public void addContact() {
+
+        System.out.println("Enter the Details of Contact Person :-");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the First Name : ");
+        String firstName = scanner.next();
+
+        List<Contact> duplicateName = contacts.stream().filter(contactPerson -> contactPerson.getFirstName().equals(firstName)).collect(Collectors.toList());
+        if (!(duplicateName.isEmpty())){
+            System.out.println("Contacts of Given First Name is already Exists !!");
+            System.out.println();
+            addContact();
+            return;
+        }
+        /*
+         * Creating Contacts for each person by Using Scanner method
+         *  add multiple contacts to address book
+         */
+        Contact contactPerson = new Contact();
+//        System.out.print("Enter the First Name : ");
+        contactPerson.setFirstName(firstName);
+        System.out.print("Enter the Last Name : ");
+        contactPerson.setLastName(scanner.next());
+        System.out.print("Enter the Address : ");
+        contactPerson.setAddress(scanner.next());
+        System.out.print("Enter the City : ");
+        contactPerson.setCity(scanner.next());
+        System.out.print("Enter the State : ");
+        contactPerson.setState(scanner.next());
+        System.out.print("Enter the Zip Code : ");
+        contactPerson.setZip(Integer.parseInt(scanner.next()));
+        System.out.print("Enter the PhoneNumber : ");
+        contactPerson.setPhoneNumber(scanner.next());
+        System.out.print("Enter the Email id : ");
+        contactPerson.setEmail(scanner.next());
+        contacts.add(contactPerson);
+    }
+    public void editContact(String searchFirstName) {
+        /*
+         * To Edit Existing Contact Person by Searching their First Name
+         * By using if Loop, details of the existing contact will print
+         * By using Switch Case, Updates the existing contact Person
+         */
+        Scanner scanner = new Scanner(System.in);
+        boolean isFound = false;
+        for (Contact contactPerson : contacts) {
+            String firstName = contactPerson.getFirstName();
+            if (firstName.equals(searchFirstName)) {
+                System.out.println("Contact Details of "+searchFirstName+" :-");
+                System.out.println("1. First Name \n2. Last Name \n3. Address \n4. City \n5. State \n6. Zip Code \n7. Phone Number \n8. Email Id \n");
+                System.out.println("Enter your Choice which you want to Edit :- ");
+                int choice = scanner.nextInt();
+
+                switch (choice){
+                    case 1:
+                        System.out.println("Update the First Name :");
+                        String updatedFirstName = scanner.next();
+                        contactPerson.setFirstName(updatedFirstName);
+                        System.out.println("First Name is UPDATED");
+                        display();
+                        break;
+                    case 2:
+                        System.out.println("Update the Last Name :");
+                        String updatedLastName = scanner.next();
+                        contactPerson.setLastName(updatedLastName);
+                        System.out.println("Last Name is UPDATED");
+                        display();
+                        break;
+                    case 3:
+                        System.out.println("Update the Address :");
+                        String updatedAddress = scanner.next();
+                        contactPerson.setAddress(updatedAddress);
+                        System.out.println("Address is UPDATED");
+                        display();
+                        break;
+                    case 4:
+                        System.out.println("Update the City :");
+                        String updatedCity = scanner.next();
+                        contactPerson.setCity(updatedCity);
+                        System.out.println("City is UPDATED");
+                        display();
+                        break;
+                    case 5:
+                        System.out.println("Update the State :");
+                        String updatedState = scanner.next();
+                        contactPerson.setState(updatedState);
+                        System.out.println("State is UPDATED");
+                        display();
+                        break;
+                    case 6:
+                        System.out.println("Update the Zip :");
+                        String updatedZip = scanner.next();
+                        contactPerson.setZip(Integer.parseInt(updatedZip));
+                        System.out.println("Zip Code is UPDATED");
+                        display();
+                        break;
+                    case 7:
+                        System.out.println("Update the Phone Number :");
+                        String updatedPhoneNumber = scanner.next();
+                        contactPerson.setPhoneNumber(updatedPhoneNumber);
+                        System.out.println("Phone Number is UPDATED");
+                        display();
+                        break;
+                    case 8:
+                        System.out.println("Update the Email Id :");
+                        String updatedEmailId = scanner.next();
+                        contactPerson.setEmail(updatedEmailId);
+                        System.out.println("Email Id is UPDATED");
+                        display();
+                        break;
+                    default:
+                        System.out.println("INVALID CHOICE");
+                        break;
+                }
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound)
+            System.out.println("No Record Found!");
+    }
+    public void deleteContact(String searchFirstName) {
+        /*
+         * To Delete the Contact Person by Searching their First Name
+         */
+        boolean isFound = false;
+        for (Contact contactPerson : contacts) {
+            if (contactPerson.getFirstName().equals(searchFirstName)) {
+                contacts.remove(contactPerson);
+                isFound = true;
+                break;
+            }
+        }
+        if (isFound) {
+            System.out.println("Contact of " + searchFirstName + " is Deleted Successfully.");
+        } else {
+            System.out.println("Contact of " + searchFirstName + " is Not Found.");
+        }
+    }
+    public void display () {
+        // To print the ArrayList of Contact
+        System.out.println(contacts);
+    }
+
+
+
+
+    public void setAddressBookName(String addressBookName) {this.addressBookName = addressBookName;
+    }
+
+    public Object getAddressBookName() {
+        return addressBookName;
+    }
+}
